9a5a59880c22b9d3981844e532a81b10d40c5a35
matteuskvx7 <kevinhernanmateus11.1@gmail.com>
2026-05-16 23:36:23 -0500

    ciclo

--- prueba.py
+++ prueba.py
@@ -1,2 +1,17 @@
 
 print("Inicio de proyecto")
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print ("hola", nombre)
+print ("tu edad es", edad)
+
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad") 
+    
+    for i in range(3):
+        print("bienvenido al sistema", nombre)
+    
+    
+        
